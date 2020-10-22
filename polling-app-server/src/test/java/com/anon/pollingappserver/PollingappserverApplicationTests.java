68c99a5d8b167eaed32d1824e33c1e742723e4f9
Đặng Hùng Vĩnh <48090305+Heisenberg5124@users.noreply.github.com>
2020-10-22 16:32:03 +0700

    Initial PollingAppServer

---
+++ polling-app-server/src/test/java/com/anon/pollingappserver/PollingappserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anon.pollingappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingappserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
